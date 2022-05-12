e55a00f59c3d21521a0f9784518dd95d051a5132
Christophe Dupriez <dupriez@destin.be>
2022-05-12 15:16:44 +0200

    Pressure measurement

--- static/js/index.js
+++ static/js/index.js
@@ -140,6 +140,8 @@ $(document).ready(function() {
                     //colorit($('#temper'),4.0,(data['watts2'] <= 0) ? data['opt_T'] : data['opt_temp'],70.0);
                     $('#rmeter').text(data['rmeter'] > 0.0 ? floorUni(data['rmeter']) : "?");
                     $('#press').text(data['press'] > 0.0 ? floorDeci(data['press']) : "?");
+                    $('#pressMin').text(data['pressMin'] > 0.0 ? floorDeci(data['pressMin']) : "?");
+                    $('#pressMax').text(data['pressMax'] > 0.0 ? floorDeci(data['pressMax']) : "?");
                     $('#extra').text(floorDeci(data['extra']));
                     $('#pumpeff').text(floorDeci(data['pumpeff']));
                     $('#heateff').text(floorDeci(data['heateff']));
