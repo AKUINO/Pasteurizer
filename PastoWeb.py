8a3f27461bf558b7b3dbc3b6d84b0eca7ff1c180
Christophe Dupriez <dupriez@destin.be>
2022-06-29 14:08:18 +0200

    Thermal disinfection program

--- PastoWeb.py
+++ PastoWeb.py
@@ -1214,7 +1214,7 @@ class Operation(object):
                         speed = self.min_speed
                 #print("SHAK="+str(speed)+"\r")
             else:
-                if menus.options['g'][3]:
+                if menus.options['g'][3] and self.sensor1 == 'warranty':
                     State.greasy = True
                 totalVol,totalTime,beginTemp,endTemp = cohorts.evolution(self.sensor2,self.sensor1)
                 #print("EVOL="+str(totalVol)+"L/"+str(totalTime)+"s "+str(beginTemp)+"°C<"+str(endTemp)+"°C\r")
@@ -1414,8 +1414,8 @@ opSequences = {
           #Operation('DetT','HEAT',ref='C',dump=False),
           #Operation('DetA','PUMP',ref='C',base_speed=MAX_SPEED,qty=TOTAL_VOL,dump=False),
           #Operation('Detr','REVR',ref='C',base_speed=MAX_SPEED,qty=-TOTAL_VOL,dump=False),
-          Operation('Deth','TRAK','output','input', base_speed=MAX_SPEED, min_speed=pumpy.minimal_liters*4, ref='C', qty=TOTAL_VOL, shake_qty=SHAKE_QTY,dump=False),
-          Operation('DetH','TRAK','output','input', base_speed=MAX_SPEED, min_speed=pumpy.minimal_liters*4, ref='C', duration=lambda:menus.options['c'][3], shake_qty=SHAKE_QTY,dump=False)
+          Operation('Deth','TRAK','output','input', base_speed=MAX_SPEED, min_speed=-pumpy.minimal_liters*6, ref='C', qty=TOTAL_VOL, shake_qty=START_VOL,dump=False),
+          Operation('DetH','TRAK','output','input', base_speed=MAX_SPEED, min_speed=-pumpy.minimal_liters*6, ref='C', duration=lambda:menus.options['c'][3], shake_qty=START_VOL,dump=False)
           #Operation('DetR','REVR',ref='C',base_speed=MAX_SPEED,qty=-TOTAL_VOL,dump=False),
           #Operation('DetS','PUMP',ref='C',base_speed=MAX_SPEED,qty=TOTAL_VOL, dump=False)
           ],
