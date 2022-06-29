6d7e0c362f8cedb4c4d5ba2b6f8f21121824c6ce
Christophe Dupriez <dupriez@destin.be>
2022-06-29 15:31:05 +0200

    Thermal disinfection program

--- PastoWeb.py
+++ PastoWeb.py
@@ -1409,15 +1409,9 @@ opSequences = {
         ],
     'C': # Désinfection thermique
         [ Operation('Dett','HEAT',ref='C',dump=False,programmable=True),
-          Operation('Detf','FILL',duration=lambda:menus.options['r'][3],base_speed=MAX_SPEED,qty=TOTAL_VOL, ref='C',dump=False),
+          Operation('DetI','FLOO',duration=lambda:menus.options['r'][3], base_speed=MAX_SPEED,qty=TOTAL_VOL, ref='C',dump=True),  #
           Operation('Dety','PAUS',message=ml.T("Entrée et Sortie connectés bout à bout","Inlet and Outlet connected end to end","Input en output aangesloten"),ref='P',dump=False),
-          #Operation('DetT','HEAT',ref='C',dump=False),
-          #Operation('DetA','PUMP',ref='C',base_speed=MAX_SPEED,qty=TOTAL_VOL,dump=False),
-          #Operation('Detr','REVR',ref='C',base_speed=MAX_SPEED,qty=-TOTAL_VOL,dump=False),
-          Operation('Deth','TRAK','output','input', base_speed=MAX_SPEED, min_speed=-pumpy.maximal_liters, ref='C', qty=TOTAL_VOL, shake_qty=TOTAL_VOL/2.1,dump=False),
-          Operation('DetH','TRAK','output','input', base_speed=MAX_SPEED, min_speed=-pumpy.maximal_liters, ref='C', duration=lambda:menus.options['c'][3], shake_qty=TOTAL_VOL/2.1,dump=False)
-          #Operation('DetR','REVR',ref='C',base_speed=MAX_SPEED,qty=-TOTAL_VOL,dump=False),
-          #Operation('DetS','PUMP',ref='C',base_speed=MAX_SPEED,qty=TOTAL_VOL, dump=False)
+          Operation('Deth','TRAK','output','input', base_speed=MAX_SPEED, min_speed=-pumpy.maximal_liters, ref='C', qty=TOTAL_VOL, shake_qty=TOTAL_VOL/2.1,dump=False)
           ],
     'N': # Détergent
         [ Operation('NetT','HEAT',ref='N',dump=False,programmable=True),
