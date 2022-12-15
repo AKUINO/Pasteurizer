7bb1e1a6c3a6a65dd05b1899e5824f3473a2a192
Christophe Dupriez <dupriez@destin.be>
2022-12-15 15:07:13 +0100

    Improvement to state transition and display

--- static/js/index.js
+++ static/js/index.js
@@ -79,7 +79,7 @@ function goToLetter(pageLetter,letter,PleaseClick) {
         $('#confirm').hide();
         $('#cancel').hide();
         console.log("/explain/"+letter+"#doc");
-        window.location.href = "/explain/"+letter+"?x="+str(Math.random())+"#doc";
+        window.location.href = "/explain/"+letter+"?x="+Math.random().ToString()+"#doc";
     }
     currentLetter = letter;
 }
