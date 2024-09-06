07f78312580534026846515efa128e7ef6593bcc
Christophe Dupriez <dupriez@destin.be>
2024-09-06 10:53:18 +0200

    Debugging Level Detection: a pull down is needed in output

--- PastoWeb.py
+++ PastoWeb.py
@@ -2234,17 +2234,23 @@ class ThreadPump(threading.Thread):
                         self.setPause(False)
 
                 if hardConf.MICHA_device and hardConf.io: # Output probably in the buffer tank
-                    # Level1 = Input (pulled HIGH = OK, in liquid), LOW = in Air: Pause!
-                    hardConf.io.write_pin(hardConf.MICHApast.LEVEL1_FLAG_REG,1) # Enable Level detection (not meaning 1=PullUp)
-                    self.level1 = hardConf.io.read_discrete(hardConf.MICHApast.LEVEL_SENSOR1_REG)
-                    if self.level1 == 0 and T_Pump.currAction in ['M','E','P','H','I']:
-                        self.setPause(True)
-                    # Level2 = Output (pulled LOW = OK, in Air, NOT in liquid), HIGH = in Liquid: Pause!
-                    hardConf.io.write_pin(hardConf.MICHApast.LEVEL2_FLAG_REG,1 if T_Pump.currAction in ['M','E','P','H','I'] else 0) # Enable Level detection if pasteurizing (not meaning 0=PULLDOWN)
-                    self.level2 = hardConf.io.read_discrete(hardConf.MICHApast.LEVEL_SENSOR2_REG)
-                    #print ("Output in liquid=%d" % self.level2)
-                    if self.level2 and T_Pump.currAction in ['M','E','P','H','I']:
-                        self.setPause(True)
+                    if T_Pump.currAction in ['M','E','P','H','I']:
+                        # Level1 = Input (pulled HIGH = OK, in liquid), LOW = in Air: Pause!
+                        hardConf.io.write_pin(hardConf.MICHApast.LEVEL1_FLAG_REG,1) # Enable Level detection (not meaning 1=PullUp)
+                        self.level1 = hardConf.io.read_discrete(hardConf.MICHApast.LEVEL_SENSOR1_REG)
+                        if self.level1 == 0:
+                            self.setPause(True)
+                        # Level2 = Output (pulled LOW = OK, in Air, NOT in liquid), HIGH = in Liquid: Pause!
+                        hardConf.io.write_pin(hardConf.MICHApast.LEVEL2_FLAG_REG,1) # Enable Level detection if pasteurizing (not meaning 0=PULLDOWN)
+                        self.level2 = hardConf.io.read_discrete(hardConf.MICHApast.LEVEL_SENSOR2_REG)
+                        #print ("Output in liquid=%d" % self.level2)
+                        if self.level2:
+                            self.setPause(True)
+                    else:
+                        hardConf.io.write_pin(hardConf.MICHApast.LEVEL1_FLAG_REG,0) # Disable Level detection
+                        self.level1 = 1
+                        hardConf.io.write_pin(hardConf.MICHApast.LEVEL2_FLAG_REG,0) # Disable Level detection
+                        self.level2 = 0
                 if Buzzer:
                     Buzzer.off()
 
